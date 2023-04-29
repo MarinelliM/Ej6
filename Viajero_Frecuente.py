5f9aa8b53f6a81a700557f3c79f5a40befe1a31d
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2023-04-29 20:31:05 -0300

    Create Viajero_Frecuente.py

---
+++ Viajero_Frecuente.py
@@ -0,0 +1,43 @@
+class Viajero:
+    num_viajero: int
+    dni: str
+    nombre: str
+    apellido: str
+    millas_acum: int
+
+
+    def __init__(self, num_viajero, dni, nombre, apellido, millas_acum) -> None:
+        self.num_viajero = num_viajero
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.millas_acum = millas_acum
+        pass
+
+    def getmillas(self):
+        return self.millas_acum
+    
+    def getnombrecompleto(self):
+        return self.nombre + ' ' +self.apellido
+
+    def acumularMillas(self, entero):
+        self.millas_acum += entero
+        print('Sus millas se acumularon con exito, sus millas actuales son {}' .format(self.getmillas()))
+
+    def getnumv(self):
+        return self.num_viajero
+    
+    def __gt__(self, otro):
+        return self.getmillas() > otro.getmillas()
+    
+    def __add__(self, otro):
+        return (self.getmillas() + otro.getmillas())
+    
+    def __sub__(self,otro):
+        return self.getmillas() - otro.getmillas()
+
+    def canjearMillas(self, millascanje):
+        if millascanje <= self.millas_acum:
+            self.millas_acum = self.millas_acum - millascanje
+            print('millas canjeadas con exito, sus millas actuales son {}' .format(self.getmillas()))
+        else: return print('Sus millas son insuficientes')
